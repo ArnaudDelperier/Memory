060c1676de11e865484827f21db91163930994c5
ArnaudDelperier <arnaud.delperier@gmail.com>
2022-01-26 16:16:25 +0100

    add getAllScores method to controller

--- back/controller.js
+++ back/controller.js
@@ -3,6 +3,30 @@ const Score = require('./models');
 
 const controller = {
 
+    getAllscores: async (req, res) => {
+        try {
+            const data = await Score.find();
+
+            // we sort the list in ascending order of score
+            const sortedData = data.sort((a, b) => +a.time - +b.time);
+
+            // cleaning unnecessary data for the front
+            const scores = sortedData.map((score, i) => {
+                return {
+                    place: i + 1,
+                    pseudo: score.pseudo,
+                    time: score.time,
+                }
+              });
+
+            res.status(200).json(scores);
+        } catch(error) {
+            console.trace(error);
+            res.status(400).json({ getAllScores: 'une erreur est survenue' });
+        }
+    },
+
+    // save new score to database
     addNewScore: async (req, res) => {
         // we deconstruct the data of the body
         let { pseudo, time } = req.body;
@@ -15,10 +39,10 @@ const controller = {
         // send score to database
         try {
           score = await score.save();
-          res.status(200).json({ save: 'ok' });
+          res.status(200).json({ addNewScore: 'ok' });
         } catch (error) {
           console.trace(error);
-          res.status(400).json({ save: 'une erreur est survenue' });
+          res.status(400).json({ addNewScore: 'une erreur est survenue' });
         }
       }
 }
--- back/router.js
+++ back/router.js
@@ -3,6 +3,7 @@ const controller = require('./controller');
 
 const router = express.Router();
 
+router.get('/get-all-scores', controller.getAllscores)
 router.post('/add-score', controller.addNewScore);
 
 module.exports = router;
